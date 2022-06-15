21b7ce6cd9c81436561a8bf42ee297c0abbdb4b6
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2022-06-15 19:12:48 +0700

    responsive fix

--- assets/javascript/responsive.js
+++ assets/javascript/responsive.js
@@ -5,17 +5,17 @@ function responsive() {
     if (width <= 739) {
         $('.container__item').parent().removeClass('col-3');
         $('.container__item').parent().removeClass('col-4');
-        $('.container__item').parent().addClass('col-6');
+        $('.container__item').parent().addClass('col-12');
     }
 
     if (width > 739 && width <= 1000) {
-        $('.container__item').parent().removeClass('col-6');
+        $('.container__item').parent().removeClass('col-12');
         $('.container__item').parent().removeClass('col-3');
         $('.container__item').parent().addClass('col-4');
     }
 
     if (width > 1000) {
-        $('.container__item').parent().removeClass('col-6');
+        $('.container__item').parent().removeClass('col-12');
         $('.container__item').parent().removeClass('col-4');
         $('.container__item').parent().addClass('col-3');
     }
